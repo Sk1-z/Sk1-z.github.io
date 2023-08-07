0011acec4e7695638228f172aa76c7e746fe2812
Schizly <marcel.the.big.cell@gmail.com>
2023-08-07 03:49:37 -0400

    Content

--- ts/main.js
+++ ts/main.js
@@ -1,11 +1,72 @@
-var _a;
-(_a = document.getElementById("opts-button-icon")) === null || _a === void 0
-  ? void 0
-  : _a.addEventListener("click", function () {
-      var sidebar = document.getElementById("sidebar");
-      if (sidebar.style.display === "none") {
+//header
+document.getElementById("opts-button-icon").addEventListener("click", function () {
+    var sidebar = document.getElementById("sidebar");
+    if (sidebar.style.display === "none") {
         sidebar.style.display = "block";
-      } else {
+    }
+    else {
         sidebar.style.display = "none";
-      }
+    }
+});
+var headers = document.getElementsByClassName("select");
+var sections = document.getElementsByClassName("content");
+function showSection(num) {
+    for (var _i = 0, sections_1 = sections; _i < sections_1.length; _i++) {
+        var section = sections_1[_i];
+        section.style.display = "none";
+    }
+    sections[num].style.display = "block";
+}
+function showHighlight(num) {
+    for (var _i = 0, headers_1 = headers; _i < headers_1.length; _i++) {
+        var header = headers_1[_i];
+        header.className = "select";
+    }
+    headers[num].className = "select selected";
+}
+var _loop_1 = function (i) {
+    headers[i].addEventListener("click", function () {
+        showSection(i);
+        showHighlight(i);
     });
+};
+for (var i = 0; i < headers.length; i++) {
+    _loop_1(i);
+}
+//entries
+var projects = sections[2];
+var games = sections[3];
+function expand(e) {
+    if (e.classList.contains("expanded")) {
+        e.className = "expand-icon";
+        e
+            .closest(".entry")
+            .getElementsByClassName("entry-content")[0].style.display = "none";
+    }
+    else {
+        e.className += " expanded";
+        e
+            .closest(".entry")
+            .getElementsByClassName("entry-content")[0].style.display = "block";
+    }
+}
+var entriesP = projects.getElementsByClassName("expand-icon");
+var _loop_2 = function (btn) {
+    btn.addEventListener("click", function () {
+        expand(btn);
+    });
+};
+for (var _i = 0, entriesP_1 = entriesP; _i < entriesP_1.length; _i++) {
+    var btn = entriesP_1[_i];
+    _loop_2(btn);
+}
+var entriesG = games.getElementsByClassName("expand-icon");
+var _loop_3 = function (btn) {
+    btn.addEventListener("click", function () {
+        expand(btn);
+    });
+};
+for (var _a = 0, entriesG_1 = entriesG; _a < entriesG_1.length; _a++) {
+    var btn = entriesG_1[_a];
+    _loop_3(btn);
+}
--- ts/main.ts
+++ ts/main.ts
@@ -1,4 +1,5 @@
-document.getElementById("opts-button-icon")?.addEventListener("click", () => {
+//header
+document.getElementById("opts-button-icon")!.addEventListener("click", () => {
   let sidebar: HTMLDivElement = document.getElementById(
     "sidebar"
   ) as HTMLDivElement;
@@ -8,3 +9,64 @@ document.getElementById("opts-button-icon")?.addEventListener("click", () => {
     sidebar.style.display = "none";
   }
 });
+
+const headers = document.getElementsByClassName("select");
+const sections = document.getElementsByClassName("content");
+
+function showSection(num: number): void {
+  for (let section of sections) {
+    section.style.display = "none";
+  }
+
+  sections[num].style.display = "block";
+}
+
+function showHighlight(num: number): void {
+  for (let header of headers) {
+    header.className = "select";
+  }
+
+  headers[num].className = "select selected";
+}
+
+for (let i = 0; i < headers.length; i++) {
+  headers[i].addEventListener("click", () => {
+    showSection(i);
+    showHighlight(i);
+  });
+}
+
+//entries
+
+const projects = sections[2];
+const games = sections[3];
+
+function expand(e: HTMLImageElement): void {
+  if (e.classList.contains("expanded")) {
+    e.className = "expand-icon";
+    e
+      .closest(".entry")!
+      .getElementsByClassName("entry-content")[0].style.display = "none";
+  } else {
+    e.className += " expanded";
+    e
+      .closest(".entry")!
+      .getElementsByClassName("entry-content")[0].style.display = "block";
+  }
+}
+
+const entriesP = projects.getElementsByClassName("expand-icon");
+
+for (let btn of entriesP) {
+  btn.addEventListener("click", () => {
+    expand(btn as HTMLImageElement);
+  });
+}
+
+const entriesG = games.getElementsByClassName("expand-icon");
+
+for (let btn of entriesG) {
+  btn.addEventListener("click", () => {
+    expand(btn as HTMLImageElement);
+  });
+}
